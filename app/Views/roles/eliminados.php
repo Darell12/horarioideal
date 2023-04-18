baa5e78a8541aa6e083eb6fe29bb5cade5df4f09
FaridMu <127342885+FaridMu@users.noreply.github.com>
2023-04-18 11:19:35 -0500

    feat:roles

--- app/Views/roles/eliminados.php
+++ app/Views/roles/eliminados.php
@@ -3,7 +3,7 @@
     <h1 class="titulo_Vista text-center"><?php echo $titulo ?></h1>
   </div>
   <div>
-    <a href="<?php echo base_url('/roles'); ?>"><button class="btn btn-outline-primary"><i class="bi bi-arrow-return-left"></i> Regresar</button></a>
+    <a href="<?php echo base_url('/ver_roles'); ?>"><button class="btn btn-outline-primary"><i class="bi bi-arrow-return-left"></i> Regresar</button></a>
   </div>
 
   <br>
@@ -25,13 +25,13 @@
         <?php } else { ?>
           <?php foreach ($datos as $valor) { ?>
             <tr>
-              <th class="text-center"><?php echo $valor['id']; ?></th>
+              <th class="text-center"><?php echo $valor['id_rol']; ?></th>
               <th class="text-center">+<?php echo $valor['nombre']; ?></th>
               <th class="text-center">
                 <?php echo $valor['estado'] = 'A' ? '<span class="text-danger"> Inactivo </span>' : '<span class="text-succes"> Inactivo </span>'; ?>
               </th>
               <th class="grid grid text-center" colspan="2">
-                <button class="btn btn-outline-warning" data-bs-toggle="modal" data-bs-target="#modal-confirma" data-href="<?php echo base_url('/roles/cambiarEstado') . '/' . $valor['id'] . '/' . 'A'; ?>" title="Restaurar"><i class="bi bi-arrow-clockwise"></i></button>
+                <button class="btn btn-outline-warning" data-bs-toggle="modal" data-bs-target="#modal-confirma" data-href="<?php echo base_url('/roles/cambiarEstado') . '/' . $valor['id_rol'] . '/' . 'A'; ?>" title="Restaurar"><i class="bi bi-arrow-clockwise"></i></button>
               </th>
 
             </tr>
@@ -41,31 +41,23 @@
     </table>
   </div>
 
-  <form method="POST" action="<?php echo base_url('/roles/Restaurar'); ?>" class="form-check-inline">
-    <div class="modal fade" id="Restaurar" tabindex="-1" aria-labelledby="Resturar" aria-hidden="true" data-bs-backdrop="static">
-      <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h1 class="modal-title fs-5 text-center" id="exampleModalLabel">¿Desea Restaurar este país?</h1>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-          </div>
-          <div class="modal-body">
-            <span>
-              <h3 class="text-center" id="RolesRestaurar"></h3>
-            </span>
-            <input type="text" id="idR" name="id" hidden>
-            <input type="text" id="estado" name="estado" hidden>
-          </div>
-          <div class="modal-footer">
-            <a href="<?php echo base_url('/paises/eliminados') ?>"><button type="button close" class="btn btn-secondary" data-bs-dismiss="modal">Close</button></a>
+  <div class="modal fade" id="modal-confirma" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+      <div class="modal-content">
+        <div style="text-align:center;" class="modal-header">
+          <h5 style="color:#98040a;font-size:20px;font-weight:bold;" class="modal-title" id="exampleModalLabel">Restauración de Registro</h5>
 
-            <!-- <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">Cancelar</button> -->
-            <button type="submit" class="btn btn-outline-success">Restaurar</button>
-          </div>
+        </div>
+        <div style="text-align:center;font-weight:bold;" class="modal-body">
+          <p>Seguro Desea Restaurar éste Registro?</p>
+        </div>
+        <div class="modal-footer">
+        <button type="button" class="btn btn-outline-primary close" data-dismiss="modal">Cancelar</button>
+          <a class="btn btn-outline-danger btn-ok">Confirmar</a>
         </div>
       </div>
     </div>
-  </form>
+  </div>
 
   <!-- Modal Elimina -->
 </div>
