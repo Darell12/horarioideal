4ae43a6f57dacbe1563c078396737ce4aa48d869
RosmyPachon <rosmypachon@gmail.com>
2023-04-24 10:28:51 -0500

    style:vistas en general

--- app/Views/principal/sidebar.php
+++ app/Views/principal/sidebar.php
@@ -116,7 +116,7 @@
 
                     <div class="bottom-content">
                         <li class="">
-                            <a href="#">
+                            <a href="<?php echo base_url('auth/logout')?>">
                                 <i class='bx bx-log-out icon'></i>
                                 <span class="text nav-text">Logout</span>
                             </a>
