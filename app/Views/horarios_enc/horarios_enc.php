55c40d2c1b68c08df284b794b05036e58d391909
darell12 <darellorlandoe@gmail.com>
2023-07-24 09:57:22 -0500

    feat:PDFonVW

--- app/Views/horarios_enc/horarios_enc.php
+++ app/Views/horarios_enc/horarios_enc.php
@@ -273,92 +273,6 @@
 </div>
 
 <script>
-    document.querySelectorAll('.button').forEach(button => {
-
-        let duration = 3000,
-            svg = button.querySelector('svg'),
-            svgPath = new Proxy({
-                y: null,
-                smoothing: null
-            }, {
-                set(target, key, value) {
-                    target[key] = value;
-                    if (target.y !== null && target.smoothing !== null) {
-                        svg.innerHTML = getPath(target.y, target.smoothing, null);
-                    }
-                    return true;
-                },
-                get(target, key) {
-                    return target[key];
-                }
-            });
-
-        button.style.setProperty('--duration', duration);
-
-        svgPath.y = 20;
-        svgPath.smoothing = 0;
-
-        button.addEventListener('click', e => {
-
-            e.preventDefault();
-
-            if (!button.classList.contains('loading')) {
-
-                button.classList.add('loading');
-
-                gsap.to(svgPath, {
-                    smoothing: .3,
-                    duration: duration * .065 / 1000
-                });
-
-                gsap.to(svgPath, {
-                    y: 12,
-                    duration: duration * .265 / 1000,
-                    delay: duration * .065 / 1000,
-                    ease: Elastic.easeOut.config(1.12, .4)
-                });
-
-                setTimeout(() => {
-                    svg.innerHTML = getPath(0, 0, [
-                        [3, 14],
-                        [8, 19],
-                        [21, 6]
-                    ]);
-                }, duration / 2);
-
-            }
-
-        });
-
-    });
-
-    function getPoint(point, i, a, smoothing) {
-        let cp = (current, previous, next, reverse) => {
-                let p = previous || current,
-                    n = next || current,
-                    o = {
-                        length: Math.sqrt(Math.pow(n[0] - p[0], 2) + Math.pow(n[1] - p[1], 2)),
-                        angle: Math.atan2(n[1] - p[1], n[0] - p[0])
-                    },
-                    angle = o.angle + (reverse ? Math.PI : 0),
-                    length = o.length * smoothing;
-                return [current[0] + Math.cos(angle) * length, current[1] + Math.sin(angle) * length];
-            },
-            cps = cp(a[i - 1], a[i - 2], point, false),
-            cpe = cp(point, a[i - 1], a[i + 1], true);
-        return `C ${cps[0]},${cps[1]} ${cpe[0]},${cpe[1]} ${point[0]},${point[1]}`;
-    }
-
-    function getPath(update, smoothing, pointsNew) {
-        let points = pointsNew ? pointsNew : [
-                [4, 12],
-                [12, update],
-                [20, 12]
-            ],
-            d = points.reduce((acc, point, i, a) => i === 0 ? `M ${point[0]},${point[1]}` : `${acc} ${getPoint(point, i, a, smoothing)}`, '');
-        return `<path d="${d}" />`;
-    }
-
     $("#formulario").validate({
         errorPlacement: function(error, element) {
             if (element[0].id == 'telUsuario') {
@@ -1017,12 +931,6 @@
         }
     }
 
-
-    // TODO: Eliminar todos los detalles anteriores
-    // TODO: Encontrar Aula disponible
-    // TODO: :(
-
-
     async function DefinirProfesores(id, idGrado) {
         let profesores = [];
         let asignaturasGrado = await ObtenerAsignaturas(idGrado); //
@@ -1322,15 +1230,11 @@
 
         for (const registro of aulaElegida) {
 
-            console.log(registro)
-
-            // franjasTotales = [];
             franjasTotalesOcupadas = [];
             franjasTotalesOcupadasLunes = [];
             franjasTotalesOcupadasAula = [];
             franjasProfesor = [];
             numeroRepeticiones = 0;
-
 
             data = [];
 
@@ -1473,50 +1377,34 @@
 
                             }
                         } catch (error) {}
-
                     }
-                    let rellenoAlerta = ''
-                    data.forEach(element => {
-                        rellenoAlerta += `<li class="lecture-time" style="text-decoration: none !important; list-style-type: none;">
-                                <div class="lecture-info">
-                                    <h6 class="lecture-title">${element.dia}</h6>
-                                    <h6 class="lecture-location">${element.hora_inicio} ~ ${element.hora_fin}</h6>
-                                </div>
-                        </li>`
-                    })
-
-                    setTimeout(() => {
-                        console.log(rellenoAlerta.length)
-                        if (rellenoAlerta.length == 0) {
-
-                            Swal.fire({
-                                title: '<h3 class="h3"> Franjas Generadas </h3>',
-                                icon: 'info',
-                                html: `No se hallaron franjas disponibles segun tus selecciones`,
-                                showCloseButton: true,
-                                focusConfirm: false,
-                                confirmButtonText: 'Intentar de nuevo!',
-                            })
-
-
-                        } else {
-                            Swal.fire({
-                                title: '<h3 class="h3"> Franjas Generadas </h3>',
-                                icon: 'info',
-                                html: `Frajas Generadas ${rellenoAlerta}
-                                            <br>
-                                            Recuerde Confirmar
-                                            `,
-                                showCloseButton: true,
-                                focusConfirm: false,
-                                confirmButtonText: 'Continuar!',
-                                confirmButtonAriaLabel: 'Thumbs up, great!',
-                            })
-                        }
-                    }, 1500);
 
                     $('#btn_enviar').removeAttr('disabled', '');
                     console.table(data)
+                    for (const element of data) {
+                        $.ajax({
+                            type: "POST",
+                            url: "<?php echo base_url('horario_det/insertar/') ?>" + element.asignatura + '/' + element.aula + '/' + element.id_dia + '/' + element.inicio + '/' + element.fin + '/' + element.id_encabezado + '/' + element.profesor + '/' + element.duracion,
+                            dataType: "json",
+                        }).done(function(data) {
+                            contador = 0
+                            let Toast = Swal.mixin({
+                                toast: true,
+                                position: 'top-end',
+                                showConfirmButton: false,
+                                timer: 3000,
+                                timerProgressBar: true,
+                                didOpen: (toast) => {
+                                    toast.addEventListener('mouseenter', Swal.stopTimer)
+                                    toast.addEventListener('mouseleave', Swal.resumeTimer)
+                                }
+                            })
+                            Toast.fire({
+                                icon: 'success',
+                                title: 'Franjas ingresadas con exito!'
+                            })
+                        });
+                    }
                 }
             })
 
